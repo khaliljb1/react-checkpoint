69697c18e44ee737a18d0ddc54c637c4df051c9d
khalil <khaliljebali2010@gmail.com>
2024-05-19 12:11:56 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <><header>
+    <a href="">React</a>
+
+    <nav>
+      <ul>
+        <li>
+          <a href="">Home</a>
+        </li>
+        <li>
+          <a href="">About</a>
+        </li>
+        <li>
+          <a href="">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+    
+    <div>
+      <h1>React</h1>
+      <p>React is faster.</p>
+      <Button variant='primary'>start learning</Button>
     </div>
+    <h1>sdsd</h1>
+  </>
   );
 }
 
